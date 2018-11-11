dcdfd5cbbe328019e04f826d87c1896b179c6454
Jack Baron <jackmbaron@gmail.com>
2018-11-11 00:47:52 +0000

    Add source

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { createServer } = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const WebSocket = require('ws')
+
+// Express App
+const app = express()
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('*', (req, res) => {
+  wss.broadcast(req.body)
+  return res.sendStatus(200)
+})
+
+// WS
+const server = createServer(app)
+const wss = new WebSocket.Server({ server })
+
+wss.broadcast = function broadcast (data) {
+  const payload = JSON.stringify(data)
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) client.send(payload)
+  })
+}
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${server.address().port} :)`)
+})
